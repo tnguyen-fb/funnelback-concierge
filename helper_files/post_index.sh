66264a7f3b44947fb2c2a0e32ce422111c86579a
Peter Levan <plevan@funnelback.com>
2017-08-11 14:35:39 +1000

    Fixed curl command to remove hardcoded collection and also quote things better

--- helper_files/post_index.sh
+++ helper_files/post_index.sh
@@ -26,7 +26,7 @@ for p in "${PROFILE[@]}"
 do
         # Run the Funnelback query to return the CSV, catching any errors.
         echo "Generating autocompletion CSV for $p"
-        curl --connect-timeout 60 --retry 3 --retry-delay 20 "http://localhost/s/search.html?collection=acp&query=!generate_autoc&profile=$p&form=auto-completion&view=$CURRENT_VIEW" -o $SEARCH_HOME/conf/$COLLECTION_NAME/$p/auto-completion.csv || exit 1
+        curl --connect-timeout 60 --retry 3 --retry-delay 20 'http://localhost:8080/s/search.html?collection='$COLLECTION_NAME'&query=!generate_autoc&profile='$p'&form=auto-completion&view='$CURRENT_VIEW'' -o $SEARCH_HOME/conf/$COLLECTION_NAME/$p/auto-completion.csv || exit 1
         # Build auto-completion using the generated auto-completion.csv
 $SEARCH_HOME/bin/build_autoc $SEARCH_HOME/data/$COLLECTION_NAME/$CURRENT_VIEW/idx/index $SEARCH_HOME/conf/$COLLECTION_NAME/$p/auto-completion.csv -collection $COLLECTION_NAME -profile $p
 
